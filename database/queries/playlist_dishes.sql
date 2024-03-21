7e867c281131013ea3241c6ac1023b8f345b9bf5
McSpicyK <loysiuskoo@gmail.com>
2024-03-21 22:35:57 +0800

    included restaurant name in sql query

--- database/queries/playlist_dishes.sql
+++ database/queries/playlist_dishes.sql
@@ -13,7 +13,7 @@ RETURNING *;
 
 
 -- name: GetPlaylistDishes :many
-SELECT d.id, d.name, d.price, p.image_url, p.date_to_be_delivered, d.cuisine, d.diet_type, p.playlist_id, r.rating, r.num_of_reviews AS number_of_reviews
+SELECT d.id, d.name, d.price, p.image_url, r.name, p.date_to_be_delivered, d.cuisine, d.diet_type, p.playlist_id, r.rating, r.num_of_reviews AS number_of_reviews
 FROM dishes d JOIN playlist_dishes p ON d.id = p.dish_id
 JOIN restaurants r ON d.restaurant_id = r.id
 WHERE p.playlist_id = $1;
